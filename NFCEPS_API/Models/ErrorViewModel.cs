4f28a8001864edc27bff8560ec3d704c61e2d393
Nirvan Timsina <somnathtimsina5@gmail.com>
2026-04-20 23:06:00 +0545

    more unnecessary files removed

--- NFCEPS_API/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace NFCEPS_API.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- NFCEPS_API/Program.cs
+++ NFCEPS_API/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
 
 // Swagger
 builder.Services.AddSwaggerGen();
